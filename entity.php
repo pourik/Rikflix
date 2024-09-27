519d31810ce92eb0e3cf775068780a1a0daf2cb3
Pourik Saha <pouriksaha2001@gmail.com>
2024-09-27 11:54:02 +0530

    Creating SesaonProvider.php script and Adding new Styles

--- entity.php
+++ entity.php
@@ -1,3 +1,15 @@
 <?php
+require_once('includes/Header.php');
+
+if(!isset($_GET["id"])){
+    ErrorMessage::show('No ID passed into page');
+}
+
+$entityId = $_GET["id"];
+$entity = new Entity($con, $entityId);
+
+$previewProvider = new PreviewProvider($con, $userLoggedIn);
+echo $previewProvider->createPreviewVideo(null);
+
 
 ?>
---
+++ includes/classes/SeasonProvider.php
@@ -0,0 +1,10 @@
+<?php
+class SeasonProvider{
+    private $con, $username;
+
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $this->username = $username;
+    }
+}
+?>
