ba2b2d7fc97fe1cfe3f2e0fa2be7214b0293a600
Pourik Saha <pouriksaha2001@gmail.com>
2024-09-28 16:45:47 +0530

    Added styles to elements in SeasonProvider.php

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -13,41 +13,48 @@ class SeasonProvider
     {
         $seasons = $entity->getSeasons();
 
-        if (sizeof($seasons) == 0) return;
+        if (sizeof($seasons) == 0) {
+            return;
+        }
 
         $seasonsHtml = "";
-        foreach($seasons as $season){
+        foreach ($seasons as $season) {
             $seasonNumber = $season->getSeasonNumber();
 
             $videosHtml = "";
-            foreach($season->getVideos() as $video){
+            foreach ($season->getVideos() as $video) {
                 $videosHtml .= $this->createVideoSquare($video);
             }
 
+
             $seasonsHtml .= "<div class='season'>
-                                <h3>Season $seasonNumber</h3>
-                                <div class='videos'>
-                                    $videosHtml
-                                </div>
-                            </div>";
+                                    <h3>Season $seasonNumber</h3>
+                                    <div class='videos'>
+                                        $videosHtml
+                                    </div>
+                                </div>";
         }
 
         return $seasonsHtml;
     }
 
-    private function createVideoSquare($video){
+    private function createVideoSquare($video)
+    {
         $id = $video->getId();
         $thumbnail = $video->getThumbnail();
-        $episodeNumber = $video->getEpisodeNumber();
         $title = $video->getTitle();
         $description = $video->getDescription();
+        $episodeNumber = $video->getEpisodeNumber();
+        // $hasSeen = $video->hasSeen($this->username) ? "<i class='fas fa-check-circle seen'></i>" : "";
 
         return "<a href='watch.php?id=$id'>
                     <div class='episodeContainer'>
-                        <img src='$thumbnail'>
-                        <div class='videoInfo'>
-                            <h4>$title</h4>
-                            <span>$description</span>
+                        <div class='contents'>
+                            <img src='$thumbnail'>
+                            <div class='videoInfo'>
+                                <h4>$episodeNumber. $title</h4>
+                                <span>$description</span>
+                            </div>
                         </div>
                     </div>
                 </a>";
