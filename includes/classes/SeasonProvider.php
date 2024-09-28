c4a01540199ae8be621942a3f70b8d2371b15450
Pourik Saha <pouriksaha2001@gmail.com>
2024-09-28 12:07:09 +0530

    Fixed Entity.php and Updated SeasonProvider.php and Season.php

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -12,5 +12,44 @@ class SeasonProvider
     public function create($entity)
     {
         $seasons = $entity->getSeasons();
+
+        if (sizeof($seasons) == 0) return;
+
+        $seasonsHtml = "";
+        foreach($seasons as $season){
+            $seasonNumber = $season->getSeasonNumber();
+
+            $videosHtml = "";
+            foreach($season->getVideos() as $video){
+                $videosHtml .= $this->createVideoSquare($video);
+            }
+
+            $seasonsHtml .= "<div class='season'>
+                                <h3>Season $seasonNumber</h3>
+                                <div class='videos'>
+                                    $videosHtml
+                                </div>
+                            </div>";
+        }
+
+        return $seasonsHtml;
+    }
+
+    private function createVideoSquare($video){
+        $id = $video->getId();
+        $thumbnail = $video->getThumbnail();
+        $episodeNumber = $video->getEpisodeNumber();
+        $title = $video->getTitle();
+        $description = $video->getDescription();
+
+        return "<a href='watch.php?id=$id'>
+                    <div class='episodeContainer'>
+                        <img src='$thumbnail'>
+                        <div class='videoInfo'>
+                            <h4>$title</h4>
+                            <span>$description</span>
+                        </div>
+                    </div>
+                </a>";
     }
 }
